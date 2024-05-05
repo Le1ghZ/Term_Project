9ae24805d5eb08cb5f48e3960c5d185492114981
Le1ghZ <163944283+Le1ghZ@users.noreply.github.com>
2024-05-05 22:38:51 +0800

    initial

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include('includes/header.php'); ?>
+
+    <h1>Hello, world!</h1>
+    <button class="btn btn-primary">Testing</button>
+    <!-- Optional Javascript; choose one of the two! -->
+    
+<?php include('includes/footer.php'); ?>
